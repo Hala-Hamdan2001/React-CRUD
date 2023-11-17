c58c902b1a5eb720138b390e066dfd04750e6ff3
hala hamdan <halahamdan@gmail.com>
2023-11-17 22:01:43 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {Routes,Route} from 'react-router-dom'
+import Index from './components/user/Index'
+import Create from './components/user/Create'
+import Details from './components/user/Details'
+import Edit from './components/user/Edit'
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path='/' element= {<Index/>} />
+      <Route path='/user/index' element= {<Index/>} />
+      <Route path='/user/:id' element= {<Details/>} />
+      <Route path='/user/edit/:id' element= {<Edit/>} />
+      <Route path='/user/create' element={<Create />} />
+      <Route path='*' element={<h1>page not found</h1>} />
+    </Routes>
+  )
+}
